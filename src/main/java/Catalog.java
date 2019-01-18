e3619177fc9c6a95091903acc1dd93fd75780d8b
Sofija Petroska <sofija_777@hotmail.com>
2019-01-18 10:24:52 +0100

    initial commit

---
+++ src/main/java/Catalog.java
@@ -0,0 +1,34 @@
+public class Catalog {
+
+    private String id;
+    private String description;
+
+    public Catalog(String id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Catalog{" +
+                "id='" + id + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
